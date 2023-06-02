fb3cc74c6cffcb26200ae4c5569e0df4415d06c4
esterarab <118554329+esterarab@users.noreply.github.com>
2023-06-02 13:59:16 -0300

    Inicio das rotas, no App.

--- brexo/App.js
+++ brexo/App.js
@@ -1,10 +1,25 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TelaLogin from './src/pages/TelaLogin'
 import TelaPrincipal from './src/pages/TelaPrincipal';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-   <TelaLogin/>
+    <NavigationContainer> 
+
+      <Stack.Screen 
+        name="TelaLogin" 
+        component={TelaLogin}
+      />
+
+      <Stack.Screen 
+        name="TelaPrincipal" 
+        component={TelaPrincipal}
+      />
+        
+    </NavigationContainer>
   );
-}
+};
 
 
