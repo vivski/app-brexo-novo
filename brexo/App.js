33bf2109b2bdfccd963fae4a28073b32c7779dd8
esterarab <118554329+esterarab@users.noreply.github.com>
2023-06-02 14:16:05 -0300

    Adicionei as navegações das telas.

--- brexo/App.js
+++ brexo/App.js
@@ -1,6 +1,11 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TelaLogin from './src/pages/TelaLogin'
 import TelaPrincipal from './src/pages/TelaPrincipal';
+import TelaCadastro from './src/pages/TelaCadastro';
+import TelaCarrinho from './src/pages/TelaCarrinho';
+import TelaFavoritos from './src/pages/TelaFavoritos';
+import TelaRecuperacaoSenha from '././src/pages/TelaRecuperacaoSenha';
+import TelaUsuario from './src/pages/TelaUsuario';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,7 +22,32 @@ export default function App() {
         name="TelaPrincipal" 
         component={TelaPrincipal}
       />
+
+      <Stack.Screen 
+        name="TelaCadastro" 
+        component={TelaCadastro}
+      />
         
+      <Stack.Screen 
+        name="TelaCarrinho" 
+        component={TelaCarrinho}
+      /> 
+
+      <Stack.Screen 
+        name="TelaFavoritos" 
+        component={TelaFavoritos}
+      />
+
+      <Stack.Screen 
+        name="TelaRecuperacaoDeSenha" 
+        component={TelaRecuperacaoSenha}
+      />
+
+      <Stack.Screen 
+        name="TelaUsuario" 
+        component={TelaUsuario}
+      />
+      
     </NavigationContainer>
   );
 };
