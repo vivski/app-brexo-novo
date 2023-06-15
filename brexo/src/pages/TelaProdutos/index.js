2199d8cb49d871cd7e80ddbb30c669ef8b7f1734
vivski <vnls1@aluno.ifal.edu.br>
2023-06-15 16:52:03 -0300

    tela de produtos
    produtos; filter;

--- brexo/src/pages/TelaProdutos/index.js
+++ brexo/src/pages/TelaProdutos/index.js
@@ -1,13 +1,60 @@
-import { View, Text } from "react-native"
+import { useEffect, useState } from "react";
+import { View, Text, Pressable } from "react-native"
+import { FlatList } from "react-native-web";
 
-function TelaProdutos(){
+function TelaProdutos({route}){
+
+const idCategoria = route.params.id;
+
+const [produtos, setProdutos] = useState([]);
+// linha pro codigo
+ const Produtos = ({nome, imagem}) => (
+<View>
+    <Pressable onPress={ () => {}}>
+    {/* navigation.navigate("TelaProdutos", {id:id})  */}
+            {/* <Image source={{uri:imagem}}/> */}
+            <Text> {nome}</Text>
+
+    </Pressable>
+</View>
+
+    );
+
+    useEffect(() =>{
+        const load = async() => {
+            const resultApi = await fetch('https://6480b615f061e6ec4d49bfea.mockapi.io/produtos')
+            const data = await resultApi.json();
+          
+            let result = data.filter(p =>p.idcategoria == idCategoria)
+                .map(({ nome, preco, imagem }) => ({
+                    nome: nome,
+                    preco: preco,
+                    imagem: imagem
+            }));
+
+            console.log(result.length);
+
+            setProdutos(result);
+        }
+
+        load();
+    }, []);
+
 return(
     <View> 
-
-        <Text> 
-            tela produtos
-        </Text>
+<View>
+    <FlatList
+        data={produtos}
+        numColumns={2}
+        renderItem={({item}) => (
+        <Produtos 
+            nome={item.nome}
+            preco={item.preco}
+            imagem={item.imagem}/>
+        )}
+    />    
+    </View>
     </View>
 )
 }
-export default TelaProdutos
+export default TelaProdutos;
