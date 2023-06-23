53303c59b53b01de32a5fe96cfd14a49a186f5e3
esterarab <118554329+esterarab@users.noreply.github.com>
2023-06-23 15:59:49 -0300

    tela produto

--- brexo/src/pages/TelaProduto/index.js
+++ brexo/src/pages/TelaProduto/index.js
@@ -1,18 +1,55 @@
-import {StyleSheet, Text, View, TextInput, Image, Pressable, FlatList} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, Pressable, FlatList } from 'react-native';
+// import Header from '../../components/Header';
 
-import Header from '../../components/Header';
+export default function TelaProduto() {
 
+    const idProduto = id;
 
+    const [produto, setProduto] = useState({});
 
-export default function TelaProduto(){
+    const Produto = ({ nome, imagem, preco }) => (
+        <View style={{ width: 200, height: 250 }}>
 
-    return(
-    <View>
-    <Header/>
-    <Text> produtos </Text>
+            <Pressable onPress={() => { }}>
+                <Image source={{ uri: imagem }} style={{ width: 150, height: 200, margin: 10 }} />
 
+                <View style={{ width: 150 }}>
+                    <Text numberOfLines={1}> {nome} </Text>
+                    <Text> {preco} </Text>
+                </View>
 
-    </View>
+            </Pressable>
+        </View>
+
+    );
+
+    useEffect(() => {
+        const load = async () => {
+            const resultApi = await fetch(`https://6480b615f061e6ec4d49bfea.mockapi.io/produtos/${idProduto}`)
+            const result = await resultApi.json();
+            setProduto(result);
+        }
+
+        load();
+    }, []);
+
+    return (
+        <View style={{ alignSelf: 'center', backgroundColor: '#F7F0F6' }}>
+            <View>
+                <Produto
+                nome={produto.nome}
+                preco={produto.preco}
+                imagem={produto.imagem}/>
+            </View>
+        </View>
     );
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+    },
+
+});
